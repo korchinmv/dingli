271754006d3e089f37f3192f2fd620cd0c90ed26
korchinmv <korchinmv@yandex.ru>
2024-11-13 17:00:16 +0300

    fix: #3

--- src/js/functions/header-height.js
+++ src/js/functions/header-height.js
@@ -1,4 +1,8 @@
 export const getHeaderHeight = () => {
-  const headerHeight = document?.querySelector('.header').offsetHeight;
-  document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
-}
+  const headerHeight = document?.querySelector(".header").offsetHeight;
+  console.log(headerHeight);
+
+  document
+    .querySelector(":root")
+    .style.setProperty("--header-height", `${headerHeight}px`);
+};
